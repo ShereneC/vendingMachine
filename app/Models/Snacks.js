61b288a7a041eb616a0e4e6447f505362884376d
ShereneC <spyhopper@cableone.net>
2021-07-13 14:38:37 -0600

    AddQuarter working

---
+++ app/Models/Snacks.js
@@ -0,0 +1,16 @@
+export default class Snacks {
+    constructor(name, price) {
+        this.name = name
+        this.price = price
+    }
+
+    //  NOTE Took out get Template to remove cards that were populating with Add Value button - trying to change Add Value to increase 'Your credit'
+    // get Template() {
+
+    //     return /*html*/`
+    //     <div class="card p-2 value">
+    //         ${this.title}
+    //     </div>
+    //     `
+    // }
+}
